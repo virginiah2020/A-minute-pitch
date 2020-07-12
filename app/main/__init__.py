87a54c3bc23246c32ebc31b48f39538e8ecb77e5
virginiah2020 <vancyvy254@gmail.com>
2020-07-12 12:29:31 +0300

    installed psycopg2 and sqlalchemy

---
+++ app/main/__init__.py
@@ -0,0 +1,17 @@
+ <div class="navbar navbar-dark bg-primary" role="navigation">
+        <div class="container">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                  <span class="sr-only">Toggle navigation</span>
+                  <span class="icon-bar"></span>
+                  <span class="icon-bar"></span>
+                  <span class="icon-bar"></span>
+                </button>
+                <ul>
+                <a style="text-align: left; color: lightblue;" class="navbar-brand" href="/pitches/new/"> PitchIn</a>
+                </ul>
+            </div>
+            <div class="navbar-nav">
+                <a style="text-align: left; color:lightblue;" class="navbar-brand" href="/"> Home </a>
+
+            </div>
