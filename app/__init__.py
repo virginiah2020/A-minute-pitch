2dd62ea4076b12e9fa6905cf10044695d29f8259
virginiah2020 <vancyvy254@gmail.com>
2020-07-12 20:43:57 +0300

    imported bootstrap

--- app/__init__.py
+++ app/__init__.py
@@ -3,3 +3,7 @@ from flask_bootstrap import Bootstrap
 from config import config_options
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
+
+bootstrap = Bootstrap()
+db = SQLAlchemy()
+login_manager = LoginManager()
