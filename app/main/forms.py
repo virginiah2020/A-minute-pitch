5041114d6c58acd7261ac9c8f8c957d26a0b390a
virginiah2020 <vancyvy254@gmail.com>
2020-07-12 20:24:17 +0300

    app route for pitch

---
+++ app/main/forms.py
@@ -0,0 +1,25 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField,TextAreaField,SubmitField,SelectField
+from wtforms.validators import Required
+
+
+class PitchForm(FlaskForm):
+    '''
+    Class to create a wtf form for creating a pitch
+    '''
+    content = TextAreaField('YOUR PITCH')
+    submit = SubmitField('SUBMIT')
+
+class CommentForm(FlaskForm):
+    '''
+    Class to create a wtf form for creating a pitch
+    '''
+    opinion = TextAreaField('WRITE COMMENT')
+    submit = SubmitField('SUBMIT')
+
+class CategoryForm(FlaskForm):
+    '''
+    Class to create a wtf form for creating a pitch
+    '''
+    name =  StringField('Category Name', validators=[Required()])
+    submit = SubmitField('Create')
